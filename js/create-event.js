9e6cb3658f5cd4b0267885dac65341dbb1fc7a71
Marcus Vinicius <marcusv.ribas@gmail.com>
2022-04-25 16:37:01 -0300

    remake create-event js while teaching and did edit event

--- js/create-event.js
+++ js/create-event.js
@@ -1,62 +1,30 @@
 const formSelector = document.querySelector('#form');
 
-console.log(formSelector);
-
 formSelector.addEventListener('submit', (event) => {
     event.preventDefault();
+
     const formObject = new FormData(formSelector);
-    console.log(formObject);
 
-    const values = [...formObject.entries()];
-    console.log(values);
+    const attractionsArray = formObject.get('attractions-input').split(',');
+    
+    const attractionsArrayTrimmed = attractionsArray.map( (attraction) => { return attraction.trim() } )
 
-    const attractionsArray = values[1][1].split(',').map((attraction) => attraction.trim());
-    console.log(attractionsArray);
+    const numberTickets = Number(formObject.get('capacity-input'))
+    
+    const body = { 
+        "name": formObject.get('name-input'),
+        "attractions": attractionsArrayTrimmed,
+        "poster": "N/D",
+        "description": formObject.get('description-input'),
+        "scheduled": formObject.get('date-input'),
+        "number_tickets": numberTickets
+     }
 
-    const body = {
-        name: values[0][1],
-        poster: "N/D",
-        attractions: attractionsArray,
-        description: values[2][1],
-        scheduled: values[3][1],
-        number_tickets: Number(values[4][1])
-    }
-
-    console.log(body);
-
-    fetch("https://xp41-soundgarden-api.herokuapp.com/events", {
-        "method": "POST",
-        "headers": {
-            "Content-Type": "application/json"
-        },
-        "body": JSON.stringify(body),
-    })
-        .then(response => {
-            console.log(response);
-        })
-        .catch(err => {
-            console.error(err);
-        });
+     fetch('https://xp41-soundgarden-api.herokuapp.com/events', {
+         "method": "POST",
+         "headers": { "Content-Type": "application/JSON" },
+         "body": JSON.stringify(body)
+     }).then( response => console.log(response)).catch( error => console.error(error));
 
 
-
-} )
-
-
-
-
-//     fetch("https://xp41-soundgarden-api.herokuapp.com/events", {
-//         "method": "POST",
-//         "headers": {
-//             "Content-Type": "application/json"
-//         },
-//         "body": JSON.stringify(body)
-//     })
-//         .then(response => {
-//             console.log(response);
-//         })
-//         .catch(err => {
-//             console.error(err);
-//         });
-// });
-
+});
---
+++ js/edit-event.js
@@ -0,0 +1,64 @@
+const formSelector = document.querySelector('#form');
+const nameSelector = document.querySelector('#name-input');
+const bannerSelector = document.querySelector('#banner-input');
+const attractionsSelector = document.querySelector('#attractions-input');
+const descriptionSelector = document.querySelector('#description-input');
+const dateSelector = document.querySelector('#date-input');
+const capacitySelector = document.querySelector('#capacity-input');
+
+const queryParameter = new URLSearchParams(window.location.search);
+
+function placeInputValuesFromEvents(data){
+    nameSelector.value = data.name;
+    bannerSelector.value = data.poster;
+    attractionsSelector.value = data.attractions.join(', ');
+    descriptionSelector.value = data.description;
+    const date = data.scheduled.substring(0,16);
+    dateSelector.value = date;
+    capacitySelector.value = data.number_tickets;
+}
+
+formSelector.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const formObject = new FormData(formSelector);
+    const attractionsArray = formObject.get('attractions-input').split(',');
+    const attractionsArrayTrimmed = attractionsArray.map( (attraction) => {return attraction.trim()} );
+
+    const numberTickets = Number(formObject.get('capacity-input'));
+
+    const body = {
+        "name": formObject.get('name-input'),
+        "poster": formObject.get('banner-input'),
+        "attractions": attractionsArrayTrimmed,
+        "description": formObject.get('description-input'),
+        "scheduled": formObject.get('date-input'),
+        "number_tickets": numberTickets
+    }
+
+    fetch("https://xp41-soundgarden-api.herokuapp.com/events/"+queryParameter.get('id'),
+        {"method": "PUT",
+         "headers": {"content-type": "application/json"},
+         "body": JSON.stringify(body)
+        })
+        .then(response => console.log(response)).catch(error => console.log(error));
+
+});
+
+
+fetch("https://xp41-soundgarden-api.herokuapp.com/events/"+queryParameter.get('id'), {
+  "method": "GET",
+  "headers": {}
+})
+.then(response => {
+  console.log(response);
+  return response.json();
+}).then(data => {
+    console.log(data);
+    placeInputValuesFromEvents(data);
+})
+.catch(err => {
+  console.error(err);
+});
+
+
