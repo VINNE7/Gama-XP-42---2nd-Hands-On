b2e4e3b9361dafaa83d4ffeb9e75bfef1cfce7a4
Marcus Vinicius <marcusv.ribas@gmail.com>
2022-04-26 01:45:15 -0300

    remake edit event js while teaching

--- js/admin-panel.js
+++ js/admin-panel.js
@@ -1,15 +1,15 @@
 const tableBodySelector = document.querySelector('#table-body');
 
-function createElementFromEvent(data){
-    data.forEach( (event, index) => {
+function createElementFromEvent(data) {
+    data.forEach((event, index) => {
         const trElement = document.createElement('tr');
 
         const thElement = document.createElement('th');
-        thElement.setAttribute('scope','row')
+        thElement.setAttribute('scope', 'row')
         thElement.innerText = index + 1;
 
         const firstTdElement = document.createElement('td');
-        const date = event.scheduled.substring(0,10).replaceAll('-', '/');
+        const date = event.scheduled.substring(0, 10).replaceAll('-', '/');
         const time = event.scheduled.substring(11, 16)
         firstTdElement.innerText = date + " " + time;
 
@@ -28,30 +28,32 @@ function createElementFromEvent(data){
 
         const secondAnchor = document.createElement('a');
         secondAnchor.innerText = "editar"
-        secondAnchor.href = "editar-evento.html?id="+event._id;
+        secondAnchor.href = "editar-evento.html?id=" + event._id;
         secondAnchor.classList.add('btn')
         secondAnchor.classList.add('btn-secondary')
 
         const thirdAnchor = document.createElement('a');
         thirdAnchor.innerText = "excluir"
-        thirdAnchor.href = "excluir-evento.html?id="+event._id;
+        thirdAnchor.href = "excluir-evento.html?id=" + event._id;
         thirdAnchor.classList.add('btn')
         thirdAnchor.classList.add('btn-danger')
 
 
 
-        fourthTdElement.append(firstAnchor,secondAnchor,thirdAnchor);
+        fourthTdElement.append(firstAnchor, secondAnchor, thirdAnchor);
         tableBodySelector.appendChild(trElement);
         trElement.append(thElement, firstTdElement, secondTdElement, thirdTdElement, fourthTdElement);
 
 
-    } );
+    });
 }
 
-fetch("https://xp41-soundgarden-api.herokuapp.com/events", {"method": "GET"}
-     ).then( response => {return response.json()}
-     ).then( data => createElementFromEvent(data)  )
-     .catch( error => console.log(error) );
+fetch("https://xp41-soundgarden-api.herokuapp.com/events", {
+        "method": "GET"
+    }).then(response => {
+        return response.json()
+    }).then(data => createElementFromEvent(data))
+    .catch(error => console.log(error));
 
 // const tableSelector = document.querySelector('.table');
 
@@ -82,7 +84,7 @@ fetch("https://xp41-soundgarden-api.herokuapp.com/events", {"method": "GET"}
 //         firstAnchor.innerText = "ver reservas";
 //         firstAnchor.classList.add('btn');
 //         firstAnchor.classList.add('btn-dark');
-        
+
 
 //         const secondAnchor = document.createElement('a');
 //         secondAnchor.innerText = "editar";
@@ -108,8 +110,3 @@ fetch("https://xp41-soundgarden-api.herokuapp.com/events", {"method": "GET"}
 // fetch('https://xp41-soundgarden-api.herokuapp.com/events', {"method": "GET"}
 // ).then(response => response.json()).then(data => createElementsFromEvents(data)
 // ).catch(error => console.log(error));
-
-
-
-
-
--- js/edit-event.js
+++ js/edit-event.js
@@ -6,59 +6,96 @@ const descriptionSelector = document.querySelector('#description-input');
 const dateSelector = document.querySelector('#date-input');
 const capacitySelector = document.querySelector('#capacity-input');
 
-const queryParameter = new URLSearchParams(window.location.search);
+const querySelector = new URLSearchParams(window.location.search);
 
-function placeInputValuesFromEvents(data){
-    nameSelector.value = data.name;
-    bannerSelector.value = data.poster;
-    attractionsSelector.value = data.attractions.join(', ');
-    descriptionSelector.value = data.description;
-    const date = data.scheduled.substring(0,16);
-    dateSelector.value = date;
-    capacitySelector.value = data.number_tickets;
+function placeInputValuesFromEvent(data){
+  nameSelector.value = data.name;
+  bannerSelector.value = data.poster;
+  attractionsSelector.value = data.attractions.join(', ');
+  descriptionSelector.value = data.description;
+  dateSelector.value = data.scheduled.substring(0,16);
+  capacitySelector.value = data.number_tickets;
 }
 
-formSelector.addEventListener('submit', (event) => {
-    event.preventDefault();
+formSelector.addEventListener('submit', event => {
+  event.preventDefault();
 
-    const formObject = new FormData(formSelector);
-    const attractionsArray = formObject.get('attractions-input').split(',');
-    const attractionsArrayTrimmed = attractionsArray.map( (attraction) => {return attraction.trim()} );
+  const body = {
+    "name": nameSelector.value,
+    "attractions": attractionsSelector.value.split(', '),
+    "poster": bannerSelector.value,
+    "description": descriptionSelector.value,
+    "scheduled": dateSelector.value,
+    "number_tickets": capacitySelector.value
+  }
 
-    const numberTickets = Number(formObject.get('capacity-input'));
-
-    const body = {
-        "name": formObject.get('name-input'),
-        "poster": formObject.get('banner-input'),
-        "attractions": attractionsArrayTrimmed,
-        "description": formObject.get('description-input'),
-        "scheduled": formObject.get('date-input'),
-        "number_tickets": numberTickets
-    }
-
-    fetch("https://xp41-soundgarden-api.herokuapp.com/events/"+queryParameter.get('id'),
-        {"method": "PUT",
-         "headers": {"content-type": "application/json"},
-         "body": JSON.stringify(body)
-        })
-        .then(response => console.log(response)).catch(error => console.log(error));
-
+  fetch('https://xp41-soundgarden-api.herokuapp.com/events/'+ querySelector.get('id'),
+  {"method": "PUT",
+  "headers": { "content-type": "application/json" },
+  "body": JSON.stringify(body)
+}
+  ).then(response => console.log(response)).catch(error => console.error(error));
+  
 });
 
+fetch('https://xp41-soundgarden-api.herokuapp.com/events/'+ querySelector.get('id'),
+  {"method": "GET"}
+).then(response => response.json()).then(data => placeInputValuesFromEvent(data)).catch(error => console.error(error));
 
-fetch("https://xp41-soundgarden-api.herokuapp.com/events/"+queryParameter.get('id'), {
-  "method": "GET",
-  "headers": {}
-})
-.then(response => {
-  console.log(response);
-  return response.json();
-}).then(data => {
-    console.log(data);
-    placeInputValuesFromEvents(data);
-})
-.catch(err => {
-  console.error(err);
-});
+
+// const queryParameter = new URLSearchParams(window.location.search);
+
+// function placeInputValuesFromEvents(data){
+//     nameSelector.value = data.name;
+//     bannerSelector.value = data.poster;
+//     attractionsSelector.value = data.attractions.join(', ');
+//     descriptionSelector.value = data.description;
+//     const date = data.scheduled.substring(0,16);
+//     dateSelector.value = date;
+//     capacitySelector.value = data.number_tickets;
+// }
+
+// formSelector.addEventListener('submit', (event) => {
+//     event.preventDefault();
+
+//     const formObject = new FormData(formSelector);
+//     const attractionsArray = formObject.get('attractions-input').split(',');
+//     const attractionsArrayTrimmed = attractionsArray.map( (attraction) => {return attraction.trim()} );
+
+//     const numberTickets = Number(formObject.get('capacity-input'));
+
+//     const body = {
+//         "name": formObject.get('name-input'),
+//         "poster": formObject.get('banner-input'),
+//         "attractions": attractionsArrayTrimmed,
+//         "description": formObject.get('description-input'),
+//         "scheduled": formObject.get('date-input'),
+//         "number_tickets": numberTickets
+//     }
+
+//     fetch("https://xp41-soundgarden-api.herokuapp.com/events/"+queryParameter.get('id'),
+//         {"method": "PUT",
+//          "headers": {"content-type": "application/json"},
+//          "body": JSON.stringify(body)
+//         })
+//         .then(response => console.log(response)).catch(error => console.log(error));
+
+// });
+
+
+// fetch("https://xp41-soundgarden-api.herokuapp.com/events/"+queryParameter.get('id'), {
+//   "method": "GET",
+//   "headers": {}
+// })
+// .then(response => {
+//   console.log(response);
+//   return response.json();
+// }).then(data => {
+//     console.log(data);
+//     placeInputValuesFromEvents(data);
+// })
+// .catch(err => {
+//   console.error(err);
+// });
 
 
