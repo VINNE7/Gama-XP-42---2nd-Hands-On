bb8fcdfaf77aca26b55e174b723bb27f94a3b529
Marcus Vinicius <marcusv.ribas@gmail.com>
2022-04-29 10:52:44 -0300

    place timeOutFunction on create event js

--- js/create-event.js
+++ js/create-event.js
@@ -26,9 +26,9 @@ formSelector.addEventListener('submit', (event) => {
     ).then(()=>{
         alert("Seu evento foi criado com sucesso");
 
-        // setTimeout(function () {
-        //     window.location.href = '/admin.html';
-        // }, 1000);
+        setTimeout(function () {
+            window.location.href = '/admin.html';
+        }, 1000);
     }
     ).catch((error)=>console.error(error));
 
